5d521e2b85fc2202b82cd94ddf970e03679ca035
luigitarallo <tarallo.gigi@gmail.com>
2024-11-14 10:33:55 +0100

    add conditional tailwind classes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,19 @@
 export default function Input({ label, invalid, ...props }) {
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let imputClasses = "w-full px-3 py-2 leading-tight border rounded shadow";
+
+  if (invalid) {
+    labelClasses += " text-red-400";
+    imputClasses += " text-red-500 bg-red-100 border-red-300";
+  } else {
+    labelClasses += " text-stone-300";
+    imputClasses += " text-gray-700 bg-stone-300";
+  }
+
   return (
     <p>
-      <label className="block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300">
-        {label}
-      </label>
-      <input
-        className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300"
-        {...props}
-      />
+      <label className={labelClasses}>{label}</label>
+      <input className={imputClasses} {...props} />
     </p>
   );
 }
